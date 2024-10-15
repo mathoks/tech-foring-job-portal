95f2804fa502bf50c6467a3cebb1b3e2084e2d97
Mathew <111003952+mathoks@users.noreply.github.com>
2024-10-15 14:55:36 +0100

    Update App1.js

--- tech-foring-job-portal-front-end/src/App.js
+++ tech-foring-job-portal-front-end/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import HomPage from './pages/homPage';
-
+import Tags from './Tags'
 function App() {
 
   return (
+    <>
+    <Tags/>
     <HomPage/>
+    </>
   )
 }
 
