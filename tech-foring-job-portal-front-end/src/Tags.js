911f15035dea49a9fd98cae0a8710f29661fcd0c
Mathew <111003952+mathoks@users.noreply.github.com>
2024-10-15 14:47:25 +0100

    Update 3Tags.js

--- tech-foring-job-portal-front-end/src/Tags.js
+++ tech-foring-job-portal-front-end/src/Tags.js
@@ -1,1 +1,30 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { SEO } from "./SEO";
 
+const Tags = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    switch (pathname) {
+      case "/vies":
+        SEO({
+          title: "welcome to tecforing",
+          metaDescription: "this is the jobs page",
+        });
+        break;
+      case "/auth/signup":
+        SEO({ title: "Sign up", metaDescription: "become a member" });
+        break;
+      case "/auth/Signin":
+        SEO({
+          title: "Sign in",
+          metaDescription: "gain access to more functionality",
+        });
+        break;
+      default:
+        break;
+    }
+  }, [pathname]);
+};
+
+export default Tags;
